8f02949a8cc7396574568740e9e99e95219c7777
panchaldeep009 <panchaldeep009@gmail.com>
2018-11-04 16:22:54 -0500

    Remove Frost Glass Effect

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
 "use strict";
 (() => {
-    html2canvas(document.querySelector("body")).then(canvas => {
-        document.querySelector(".frostGlass > .backgroundImage").style.backgroundImage = `url(${canvas.toDataURL('image/jpeg', 1.0)})`;
-    });
-
-    document.addEventListener("resize", function(){
-        html2canvas(document.querySelector("body")).then(canvas => {
-            document.querySelector(".frostGlass > .backgroundImage").style.backgroundImage = `url(${canvas.toDataURL('image/jpeg', 1.0)})`;
-        });
-    });
-
-    document.addEventListener("scroll", function(){
-        document.querySelector(".frostGlass > .backgroundImage")
-            .style.backgroundPosition = 
-            `0px -${document.documentElement.scrollTop}px`;
-    });
-    
-    document.addEventListener("mousemove", function(e){
-        document.querySelector(".frostGlass").style.top = e.clientY-100+"px";
-        document.querySelector(".frostGlass").style.left = e.clientX-100+"px";
-    });
-
 })();
