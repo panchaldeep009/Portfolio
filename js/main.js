22b196915dcbb7e1972f1883a427f0d61d43453c
panchaldeep009 <panchaldeep009@gmail.com>
2018-11-04 20:35:08 -0500

    Logo animation added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 "use strict";
 (() => {
+    // Logo animation
+    [
+        // Code Section
+        document.querySelector('#codeSection'),
+        document.querySelector('#designSection'),
+    ].forEach(section => {
+        // Selecting logo and animate stroke
+        section.querySelector('.logoSVG')
+            .addEventListener('load', function(){
+                
+                this.contentDocument
+                    .querySelectorAll('svg polyline, svg circle, svg path')
+                    .forEach(svgElement => {
+                        svgElement.style['stroke-dasharray'] = 
+                        svgElement.style['stroke-dashoffset'] =
+                            svgElement.getTotalLength();
+                        svgElement.style.stroke = 
+                            section.id == 'codeSection' ?
+                            "#e5e5e5" : "#292d3e";
+                    });
+
+                setTimeout(() => {
+                    this.contentDocument
+                        .querySelectorAll('#centerLine')
+                        .forEach(svgElement => {
+                            svgElement.classList.add('dashoffsetTransition');
+                            svgElement.style.strokeDashoffset = 0;
+                        });
+                }, 500);
+
+                setTimeout(() => {
+                    this.contentDocument
+                        .querySelectorAll('svg circle, svg path')
+                        .forEach(svgElement => {
+                            svgElement.classList.add('dashoffsetTransition');
+                            svgElement.style.strokeDashoffset = 0;
+                        });
+                }, 1000);
+            }, false);
+    })
 })();
