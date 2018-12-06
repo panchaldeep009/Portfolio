a099635698508a9394b9f893d2a09194ddc425b9
panchaldeep009 <panchaldeep009@gmail.com>
2018-12-06 10:56:25 -0500

    ligthbox done

--- js/gallery.js
+++ js/gallery.js
@@ -44,73 +44,103 @@
         thumbs[0].parentElement.dataset.activeThumb = newThumbI;
     }
     function initializeGrid(thumbs){
-        let parentE = thumbs[0].parentElement;
-        parentE.dataset.activeThumb = 0;
-        parentE.dataset.xState = 0;
-        parentE.dataset.yState = 0;
-        thumbs.forEach(item => {
-            item.style.top = generateRandomNumber(2 , 90)+"%"; 
-            item.style.left = generateRandomNumber(2 , 90)+"%";
-            item.style.transform = `translate3d(0,0,0) rotateY(-180deg) rotate(${generateRandomNumber(-30 , 30)}deg)`;
-        });
-        nextThumbs(thumbs);
-        parentE.addEventListener('mousedown', function(e){
-            parentE.dataset.xState = e.clientX;
-            parentE.dataset.yState = e.clientY;
-        });
-        parentE.addEventListener('mousemove', function(e){
-            if(parentE.dataset.yState != 0){
-                if(Math.abs(parentE.dataset.xState - e.clientX) < 60){
-                    if((parentE.dataset.yState - e.clientY) > 50){
-                        nextThumbs(thumbs);
-                        parentE.dataset.xState = e.clientX;
-                        parentE.dataset.yState = e.clientY;
-                    } else if((parentE.dataset.yState - e.clientY) < -50){
-                        preThumbs(thumbs);
-                        parentE.dataset.xState = e.clientX;
-                        parentE.dataset.yState = e.clientY;
+        if(thumbs != undefined) {
+            let parentE = thumbs[0].parentElement;
+            parentE.dataset.activeThumb = 0;
+            parentE.dataset.xState = 0;
+            parentE.dataset.yState = 0;
+            thumbs.forEach(item => {
+                item.style.top = generateRandomNumber(2 , 90)+"%"; 
+                item.style.left = generateRandomNumber(2 , 90)+"%";
+                item.style.transform = `translate3d(0,0,0) rotateY(-180deg) rotate(${generateRandomNumber(-30 , 30)}deg)`;
+            });
+            nextThumbs(thumbs);
+            parentE.addEventListener('mousedown', function(e){
+                parentE.dataset.xState = e.clientX;
+                parentE.dataset.yState = e.clientY;
+            });
+            parentE.addEventListener('mousemove', function(e){
+                if(parentE.dataset.yState != 0){
+                    if(Math.abs(parentE.dataset.xState - e.clientX) < 60){
+                        if((parentE.dataset.yState - e.clientY) > 50){
+                            nextThumbs(thumbs);
+                            parentE.dataset.xState = e.clientX;
+                            parentE.dataset.yState = e.clientY;
+                        } else if((parentE.dataset.yState - e.clientY) < -50){
+                            preThumbs(thumbs);
+                            parentE.dataset.xState = e.clientX;
+                            parentE.dataset.yState = e.clientY;
+                        }
                     }
                 }
-            }
-        });
-        parentE.addEventListener('mouseup', function(){
-            if(parentE.dataset.yState != 0){
-                parentE.dataset.xState = 0;
-                parentE.dataset.yState = 0;
-            }
-        });
-        parentE.addEventListener('touchstart', function(e){
-            parentE.dataset.xState = e.touches[0].clientX;
-            parentE.dataset.yState = e.touches[0].clientY;
-        });
-        parentE.addEventListener('touchmove', function(e){
-            if(parentE.dataset.yState != 0){
-                if(Math.abs(parentE.dataset.xState - e.touches[0].clientX) < 60){
-                    if((parentE.dataset.yState - e.touches[0].clientY) > 50){
-                        nextThumbs(thumbs);
-                        parentE.dataset.xState = e.touches[0].clientX;
-                        parentE.dataset.yState = e.touches[0].clientY;
-                    } else if((parentE.dataset.yState - e.touches[0].clientY) < -50){
-                        preThumbs(thumbs);
-                        parentE.dataset.xState = e.touches[0].clientX;
-                        parentE.dataset.yState = e.touches[0].clientY;
+            });
+            parentE.addEventListener('mouseup', function(){
+                if(parentE.dataset.yState != 0){
+                    parentE.dataset.xState = 0;
+                    parentE.dataset.yState = 0;
+                }
+            });
+            parentE.addEventListener('touchstart', function(e){
+                parentE.dataset.xState = e.touches[0].clientX;
+                parentE.dataset.yState = e.touches[0].clientY;
+            });
+            parentE.addEventListener('touchmove', function(e){
+                if(parentE.dataset.yState != 0){
+                    if(Math.abs(parentE.dataset.xState - e.touches[0].clientX) < 60){
+                        if((parentE.dataset.yState - e.touches[0].clientY) > 50){
+                            nextThumbs(thumbs);
+                            parentE.dataset.xState = e.touches[0].clientX;
+                            parentE.dataset.yState = e.touches[0].clientY;
+                        } else if((parentE.dataset.yState - e.touches[0].clientY) < -50){
+                            preThumbs(thumbs);
+                            parentE.dataset.xState = e.touches[0].clientX;
+                            parentE.dataset.yState = e.touches[0].clientY;
+                        }
                     }
                 }
-            }
-        });
-        parentE.addEventListener('touchend', function(){
-            if(parentE.dataset.yState != 0){
-                parentE.dataset.xState = 0;
-                parentE.dataset.yState = 0;
-            }
-        });
+            });
+            parentE.addEventListener('touchend', function(){
+                if(parentE.dataset.yState != 0){
+                    parentE.dataset.xState = 0;
+                    parentE.dataset.yState = 0;
+                }
+            });
+        }
     }
     
-    
+    var fetchModel = [
+        {
+            "item_id": "",
+            "item_name": "",
+            "item_about": "",
+            "item_date": "",
+            "item_resource": "",
+            "item_main_category": "",
+            "media": [
+                {
+                    "media_src": "",
+                    "media_type": "",
+                    "media_size": "",
+                    "media_caption": ""
+                }
+            ],
+            "sub_category": [
+                {
+                    "sub_category_id": "",
+                    "sub_category_name": ""
+                }
+            ],
+            "technology": [
+                {
+                    "technology_id": "",
+                    "technology_name": ""
+                }
+            ]
+        }];
     var codePortfolio = new Vue({
         el: '#codePortfolio',
         data: {
-            mainData: [],
+            mainData: fetchModel,
             galleryData: [],
         },
         methods: {
@@ -132,17 +162,28 @@
     var codeLightBox = new Vue({
         el: '#codeLightBox',
         data: {
-            mainData: [],
+            mainData: fetchModel,
+            i: 0,
+            preMedia: null,
+            nxtMedia: null,
             lightBoxClass: 'lightBox hideBox',
         },
         methods: {
             open: function(id){
                 this.mainData = codePortfolio.mainData.filter(item => item.item_id == id)[0];
                 this.lightBoxClass = 'lightBox';
+                this.preMedia = (this.mainData.media[this.i-1] != undefined) ? true : null;
+                this.nxtMedia = (this.mainData.media[this.i+1] != undefined) ? true : null;
             },
             close: function(){
                 this.lightBoxClass = 'lightBox hideBox';
                 window.location.href = "#/code/work";
+            }
+        },
+        watch: {
+            i: function(){
+                this.preMedia = (this.mainData.media[this.i-1] != undefined) ? true : null;
+                this.nxtMedia = (this.mainData.media[this.i+1] != undefined) ? true : null;
             }
         }
     });
@@ -151,7 +192,7 @@
     var designPortfolio = new Vue({
         el: '#graphicsPortfolio',
         data: {
-            mainData: [],
+            mainData: fetchModel,
             galleryData: [],
         },
         methods: {
@@ -173,7 +214,8 @@
     var designLightBox = new Vue({
         el: '#designLightBox',
         data: {
-            mainData: [],
+            mainData: fetchModel,
+            i: 0,
             lightBoxClass: 'lightBox hideBox',
         },
         methods: {
